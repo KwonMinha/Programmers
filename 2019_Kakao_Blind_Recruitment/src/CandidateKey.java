34b51ee1cad1aa702d188bd4342b45c43f497e78
Minha Kwon <minha61@naver.com>
2021-01-11 04:13:52 +0900

    Create CandidateKey.java

---
+++ 2019_Kakao_Blind_Recruitment/src/CandidateKey.java
@@ -0,0 +1,13 @@
+
+public class CandidateKey {
+
+	public static void main(String[] args) {
+		String[][] relation = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}};
+		solution(relation);
+	}
+
+	 public static int solution(String[][] relation) {
+	        int answer = 0;
+	        return answer;
+	    }
+}
