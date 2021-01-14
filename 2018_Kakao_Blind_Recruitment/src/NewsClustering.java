9165584c31e2f19428528f26eb923c3c392bc19d
Minha Kwon <minha61@naver.com>
2021-01-14 23:26:34 +0900

    Create NewsClustering.java

---
+++ 2018_Kakao_Blind_Recruitment/src/NewsClustering.java
@@ -0,0 +1,23 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 11.
+ * 
+ * 2018 카카오 신입 공채 - [1차] 뉴스 클러스터링
+ * https://programmers.co.kr/learn/courses/30/lessons/17677
+ */
+
+public class NewsClustering {
+
+	public static void main(String[] args) {
+		String str1 = "FRANCE";
+		String str2 = "french";
+		
+		solution(str1, str2);
+	}
+	
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+
+}
