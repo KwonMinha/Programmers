bdba671c35edbf7abfb96b6f06d740e84481d347
Minha Kwon <minha61@naver.com>
2021-01-29 04:21:53 +0900

    Create NewID.java

---
+++ 2021_Kakao_Blind_Recruitment/src/NewID.java
@@ -0,0 +1,24 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 29.
+ * 
+ * 2020 카카오 공채 - 신규 아이디 추천 
+ * https://programmers.co.kr/learn/courses/30/lessons/72410
+ */
+
+import java.util.*;
+
+public class NewID {
+
+	public static void main(String[] args) {
+		String new_id = "...!@BaT#*..y.abcdefghijklm";
+		
+		System.out.println(solution(new_id));
+	}
+	
+    public static String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+
+}
