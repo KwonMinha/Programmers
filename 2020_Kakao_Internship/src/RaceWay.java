dd5e06f475d1c9dfac79029e9f4a5314597d06c0
Minha Kwon <minha61@naver.com>
2021-01-30 22:34:25 +0900

    Create RaceWay.java

---
+++ 2020_Kakao_Internship/src/RaceWay.java
@@ -0,0 +1,24 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 1. 30.
+ * 
+ * 2020 카카오 인턴십 - 경주로 건설
+ * https://programmers.co.kr/learn/courses/30/lessons/67259
+ */
+
+public class RaceWay {
+
+	public static void main(String[] args) {
+		int[][] board = {{0,0,0,0,0,0,0,1},{0,0,0,0,0,0,0,0},{0,0,0,0,0,1,0,0},
+				{0,0,0,0,1,0,0,0},{0,0,0,1,0,0,0,1},{0,0,1,0,0,0,1,0},
+				{0,1,0,0,0,1,0,0},{1,0,0,0,0,0,0,0}};
+		
+		System.out.println(solution(board));
+	}
+	
+	public  static int solution(int[][] board) {
+        int answer = 0;
+        return answer;
+    }
+
+}
