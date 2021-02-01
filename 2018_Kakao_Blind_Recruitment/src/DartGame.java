3470c4783ae1f74e1c6fd59c30fc78870f1b65a6
Minha Kwon <minha61@naver.com>
2021-02-01 19:34:45 +0900

    Update DartGame.java

--- 2018_Kakao_Blind_Recruitment/src/DartGame.java
+++ 2018_Kakao_Blind_Recruitment/src/DartGame.java
@@ -1,9 +1,22 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 1.
+ * 
+ * 2018 카카오 신입 공채 - [1차] 다트 게임 
+ * https://programmers.co.kr/learn/courses/30/lessons/17682
+ */
 
 public class DartGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String dartResult = "1S2D*3T";
+	
+		System.out.println(solution(dartResult));
 
 	}
 
+	public static int solution(String dartResult) {
+        int answer = 0;
+        return answer;
+    }
 }
