174483e5a56d6d756ce5bbb5d4a8e6c0a4773043
Minha Kwon <minha61@naver.com>
2021-02-05 23:14:00 +0900

    Create SteppingStoneBridge.java

---
+++ 2019_Kakao_Blind_Recruitment/src/SteppingStoneBridge.java
@@ -0,0 +1,25 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 5.
+ * 
+ * 2019 카카오 신입 공채 - 징검다리 건너기    
+ * https://programmers.co.kr/learn/courses/30/lessons/64062
+ */
+
+import java.util.*;
+
+public class SteppingStoneBridge {
+
+	public static void main(String[] args) {
+		int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+		int k = 3;
+		
+		System.out.println(solution(stones, k)); // 3
+	}
+	
+	public static int solution(int[] stones, int k) {
+        int answer = 0;
+        return answer;
+    }
+
+}
