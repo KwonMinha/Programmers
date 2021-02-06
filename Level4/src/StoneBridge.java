6231589c634916835e22e7fccb0d5c2a3d32d4ef
Minha Kwon <minha61@naver.com>
2021-02-07 01:57:09 +0900

    Create StoneBridge.java

---
+++ Level4/src/StoneBridge.java
@@ -0,0 +1,22 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 7.
+ * 
+ * Level4 - 이분 탐색 - 징검다리   
+ * https://programmers.co.kr/learn/courses/30/lessons/43236
+ */
+
+public class StoneBridge {
+
+	public static void main(String[] args) {
+		int[] rocks = {2, 14, 11, 21, 17};
+		System.out.println(solution(25, rocks, 2));
+	}
+	
+	public static int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
+        
+        return answer;
+    }
+
+}
