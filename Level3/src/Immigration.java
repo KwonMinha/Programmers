9aef23528af7f55e74c97d50b2e44037dbeb7dcd
Minha Kwon <minha61@naver.com>
2021-02-07 01:33:55 +0900

    Update Immigration.java

--- Level3/src/Immigration.java
+++ Level3/src/Immigration.java
@@ -12,11 +12,43 @@ public class Immigration {
 		int[] times = {7, 10};
 		System.out.println(solution(6, times)); // 28
 	}
-	
+
 	public static long solution(int n, int[] times) {
-        long answer = 0;
-        
-        return answer;
-    }
+		long answer = Long.MAX_VALUE;
+		long low = 1;
+		long high = 1000000000;
+		long mid = 0;
+
+		while(low <= high) {
+			mid = (low + high) / 2;
+
+			if(count(times, n, mid)) {
+				high = mid - 1;
+				answer = Math.min(answer, mid);
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return answer;
+	}
+
+	public static boolean count(int[] times, int n, long mid) {
+		long cnt = 0;
+
+		for(int i = 0; i < times.length; i++) {
+			long time = times[i];
+			while(time <= mid) {
+				cnt++;
+				time += times[i];
+			}
+
+			if(cnt >= n) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 
 }
