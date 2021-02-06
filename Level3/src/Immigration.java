9cd116c025dfe0f22d864b79c614a4706dad1ab3
Minha Kwon <minha61@naver.com>
2021-02-07 00:47:45 +0900

    Create Immigration.java

---
+++ Level3/src/Immigration.java
@@ -0,0 +1,22 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 2. 6.
+ * 
+ * Level3 - 이분 탐색 - 입국 심사  
+ * https://programmers.co.kr/learn/courses/30/lessons/43238
+ */
+
+public class Immigration {
+
+	public static void main(String[] args) {
+		int[] times = {7, 10};
+		System.out.println(solution(6, times)); // 28
+	}
+	
+	public static long solution(int n, int[] times) {
+        long answer = 0;
+        
+        return answer;
+    }
+
+}
