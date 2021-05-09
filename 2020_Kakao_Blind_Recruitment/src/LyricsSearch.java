177f57e3c0ce650dd4ac59320995d4b148ff81ba
Minha Kwon <minha61@naver.com>
2021-05-09 23:19:50 +0900

    가사 검색
    Not Solved

---
+++ 2020_Kakao_Blind_Recruitment/src/LyricsSearch.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LyricsSearch {
+
+	public static void main(String[] args) {
+		String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+		String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
+
+		int[] ans = solution(words, queries);
+		System.out.println(Arrays.toString(ans));
+
+	}
+
+	public static int[] solution(String[] words, String[] queries) {
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		int[] answer = new int[queries.length];
+
+		//ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(words));
+
+		for(int i = 0; i < queries.length; i++) {
+			int startIdx = queries[i].indexOf("?");
+			int lastIdx = queries[i].lastIndexOf("?");
+
+			StringBuilder sb = new StringBuilder();
+			String q = "";
+			int qLen = queries[i].length();
+			int flag = 0;
+
+			if(startIdx == 0) { // ?가 접두사로 올 경우
+				flag = 0;
+				q = queries[i].substring(lastIdx+1);
+				sb.append(queries[i].substring(lastIdx+1));
+			} else { // ?가 접미사로 올 경우
+				flag = 1;
+				q = queries[i].substring(0, startIdx);
+				sb.append(queries[i].substring(0, startIdx));
+			}
+
+			//String key = qLen + flag + q;
+			String key = qLen + flag + sb.toString();
+
+			if(hashMap.containsKey(key)) {
+				answer[i] = hashMap.get(key);
+			} else {
+				int count = 0;
+				
+				for(int j = 0; j < words.length; j++) {
+					String word = words[j];
+					
+					if(word.length() != qLen) continue;
+					
+					if(flag == 0 && word.endsWith(sb.toString())) {
+						count++;
+					} else if(flag == 1 && word.startsWith(sb.toString())) {
+						count++;
+					}
+				}
+				
+				answer[i] = count;
+				hashMap.put(key, count);
+			}
+		}
+
+		return answer;
+	}
+
+}
