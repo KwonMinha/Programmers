552b06c092575c936317fa03dc291ab643b4eb3e
Minha Kwon <minha61@naver.com>
2021-05-16 22:03:00 +0900

    Create MakeBigNumber.java

---
+++ Level2/src/MakeBigNumber.java
@@ -0,0 +1,14 @@
+
+public class MakeBigNumber {
+
+	public static void main(String[] args) {
+		System.out.println(solution("1924", 2));
+	}
+	
+	 public static String solution(String number, int k) {
+	        String answer = "";
+	        
+	        return answer;
+	    }
+
+}
