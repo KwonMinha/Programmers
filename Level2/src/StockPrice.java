20880f94f2318c6021eb4e546971a0cf2d80c36a
Minha Kwon <minha61@naver.com>
2021-06-10 21:53:08 +0900

    주식 가격
    Update

--- Level2/src/StockPrice.java
+++ Level2/src/StockPrice.java
@@ -18,18 +18,27 @@ public class StockPrice {
 	public static int[] solution(int[] prices) {
 		int[] answer = new int[prices.length];
 
+//		for(int i = 0; i < prices.length; i++) {
+//			int cnt = 0;
+//			
+//			for(int j = i+1; j < prices.length; j++) {
+//				if(prices[i] > prices[j]) {
+//					cnt++;
+//					break;
+//				} else {
+//					cnt++;
+//				}
+//			}
+//			answer[i] = cnt;
+//		}
+		
 		for(int i = 0; i < prices.length; i++) {
-			int cnt = 0;
-			
 			for(int j = i+1; j < prices.length; j++) {
+				answer[i]++;
 				if(prices[i] > prices[j]) {
-					cnt++;
 					break;
-				} else {
-					cnt++;
-				}
+				} 
 			}
-			answer[i] = cnt;
 		}
 
 		return answer;
