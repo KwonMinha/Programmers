b69361179f5e4aba8a054369987e80a438ad1c36
Minha Kwon <minha61@naver.com>
2021-06-11 19:26:42 +0900

    Update MaximumNumber.java

--- Level2/src/MaximumNumber.java
+++ Level2/src/MaximumNumber.java
@@ -8,10 +8,6 @@
 import java.util.*;
 
 public class MaximumNumber {
-	static boolean[] visited;
-	static int[] output;
-	static int max = 0;
-	static String maxStr = "";
 
 	public static void main(String[] args) {
 		int[] numbers = {3, 30, 34, 5, 9};
@@ -19,40 +15,31 @@ public class MaximumNumber {
 	}
 
 	public static String solution(int[] numbers) {
-		int n = numbers.length;
-
-		visited = new boolean[n];
-		output = new int[n];
-
-		permutation(n, n, 0, numbers);
-
-		return maxStr;
-	}
-
-	static void permutation(int r, int n, int depth, int[] numbers) {
-		if(depth == r) {
-			String str = "";
-
-			for(int i = 0; i < output.length; i++) {
-				str += output[i];
-			}
-
-			if(max < Integer.parseInt(str)) {
-				max = Integer.parseInt(str);
-				maxStr = str;
-			}
-
-			return;
+		String[] num = new String[numbers.length];
+		for(int i = 0; i < numbers.length; i++) {
+			num[i] = String.valueOf(numbers[i]);
 		}
 
-		for(int i = 0; i < n; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				output[depth] = numbers[i];
-				permutation(r, n, depth + 1, numbers);
-				visited[i] = false;
+		Arrays.sort(num, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				int n1 = Integer.parseInt(o1+o2);
+				int n2 = Integer.parseInt(o2+o1);
+
+				if(n1 <= n2) {
+					return 1;
+				} else {
+					return -1;
+				}
 			}
+		});
+		
+		String answer = "";
+		for(int i = 0; i < num.length; i++) {
+			answer += num[i];
 		}
+
+		return answer;
 	}
 
 }
