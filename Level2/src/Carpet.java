b92d6c8852e405252fc6e8de70089af971248a76
Minha Kwon <minha61@naver.com>
2021-06-16 21:15:28 +0900

    Create Carpet.java

---
+++ Level2/src/Carpet.java
@@ -0,0 +1,22 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 6. 16.
+ * Level2 - 완전탐색 - 카펫 
+ * https://programmers.co.kr/learn/courses/30/lessons/42842
+ */
+
+import java.util.*;
+
+public class Carpet {
+
+	public static void main(String[] args) {
+		int[] ans = solution(10, 2);
+		System.out.println(Arrays.toString(ans));
+	}
+
+	 public static int[] solution(int brown, int yellow) {
+	        int[] answer = {};
+	        return answer;
+	    }
+
+}
