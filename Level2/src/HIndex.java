9ea67b738baa4479d683634f2d6733dc1fdcbd11
Minha Kwon <minha61@naver.com>
2021-06-16 19:19:20 +0900

    Create HIndex.java

---
+++ Level2/src/HIndex.java
@@ -0,0 +1,20 @@
+/**
+ * @author Minha Gwon
+ * @date 2021. 6. 16.
+ * Level2 - 정렬 - H-Index
+ * https://programmers.co.kr/learn/courses/30/lessons/42747
+ */
+
+public class HIndex {
+
+	public static void main(String[] args) {
+		int[] citations = {3, 0, 6, 1, 5};
+		System.out.println(solution(citations));
+	}
+	
+	public static int solution(int[] citations) {
+        int answer = 0;
+        return answer;
+    }
+
+}
